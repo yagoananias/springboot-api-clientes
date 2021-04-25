e11f48fc42737f25d036746c4901a6ab8b5d4013
Yago Ananias <yago_souza92@yahoo.com.br>
2021-04-25 18:24:39 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/yagoananias/apiclientes/SpringbootApiClientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.yagoananias.apiclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/yagoananias/apiclientes/SpringbootApiClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yagoananias.apiclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
