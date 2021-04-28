cea0711d46c06bc045702bdca49b13b82a8e2375
Yago Ananias <yago_souza92@yahoo.com.br>
2021-04-28 17:29:49 -0300

    Add banco de dados H2

--- src/main/java/br/com/yagoananias/apiclientes/domain/Categoria.java
+++ src/main/java/br/com/yagoananias/apiclientes/domain/Categoria.java
@@ -2,35 +2,45 @@ package br.com.yagoananias.apiclientes.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
 	public Categoria() {
 	}
-
+	
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
 	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 	public Integer getId() {
 		return id;
 	}
-
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -38,7 +48,7 @@ public class Categoria implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -55,9 +65,4 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-
 }
