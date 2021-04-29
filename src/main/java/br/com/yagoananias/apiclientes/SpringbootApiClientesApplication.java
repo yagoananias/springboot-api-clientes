9fff90fade306b6850a1682b51bc895f3fdb9d77
Yago Ananias <yago_souza92@yahoo.com.br>
2021-04-29 14:03:24 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/yagoananias/apiclientes/SpringbootApiClientesApplication.java
+++ src/main/java/br/com/yagoananias/apiclientes/SpringbootApiClientesApplication.java
@@ -1,13 +1,32 @@
 package br.com.yagoananias.apiclientes;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.yagoananias.apiclientes.domain.Categoria;
+import br.com.yagoananias.apiclientes.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringbootApiClientesApplication {
+public class SpringbootApiClientesApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApiClientesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
