d1d30e6f60341c90ed98000a7600e9af0745cb4e
Yago Ananias <yago_souza92@yahoo.com.br>
2021-04-30 03:00:02 -0300

    Produto e associacao muitos para muitos

--- src/main/java/br/com/yagoananias/apiclientes/SpringbootApiClientesApplication.java
+++ src/main/java/br/com/yagoananias/apiclientes/SpringbootApiClientesApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.yagoananias.apiclientes.domain.Categoria;
+import br.com.yagoananias.apiclientes.domain.Produto;
 import br.com.yagoananias.apiclientes.repositories.CategoriaRepository;
+import br.com.yagoananias.apiclientes.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringbootApiClientesApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApiClientesApplication.class, args);
@@ -26,7 +31,19 @@ public class SpringbootApiClientesApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
