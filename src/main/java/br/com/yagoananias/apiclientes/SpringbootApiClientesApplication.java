79de3164dcdacf5024be53764edc387f429da752
Yago Ananias <yago_souza92@yahoo.com.br>
2021-04-30 14:56:48 -0300

    Estado e Cidade

--- src/main/java/br/com/yagoananias/apiclientes/SpringbootApiClientesApplication.java
+++ src/main/java/br/com/yagoananias/apiclientes/SpringbootApiClientesApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.yagoananias.apiclientes.domain.Categoria;
+import br.com.yagoananias.apiclientes.domain.Cidade;
+import br.com.yagoananias.apiclientes.domain.Estado;
 import br.com.yagoananias.apiclientes.domain.Produto;
 import br.com.yagoananias.apiclientes.repositories.CategoriaRepository;
+import br.com.yagoananias.apiclientes.repositories.CidadeRepository;
+import br.com.yagoananias.apiclientes.repositories.EstadoRepository;
 import br.com.yagoananias.apiclientes.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class SpringbootApiClientesApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApiClientesApplication.class, args);
@@ -44,6 +54,21 @@ public class SpringbootApiClientesApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
 	}
 
 }
